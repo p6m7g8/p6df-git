6c154ada2597e0bebb174371ad3a4dd059cba980
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 15:02:59 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,13 @@
+p6df::modules::git::version() { echo "0.0.1" }
+p6df::modules::git::deps()    { 
+	ModuleDeps=(
+		sorin-ionescu/prezto:modules/git
+	)
+}
+
+p6df::modules::git::external() { }
+
+p6df::modules::git::init() {
+}
+
+p6df::modules::git::init
