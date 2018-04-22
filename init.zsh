44bc99bff1997d4bf6a33d977a0d4c23dee05027
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-21 21:35:50 -0400

    brew

--- init.zsh
+++ init.zsh
@@ -2,10 +2,20 @@ p6df::modules::git::version() { echo "0.0.1" }
 p6df::modules::git::deps()    { 
 	ModuleDeps=(
 		sorin-ionescu/prezto:modules/git
+		p6m7g8/p6git
 	)
 }
 
-p6df::modules::git::external() { }
+p6df::modules::git::external::brew() { 
+
+  brew install git --without-completions
+  brew install git-extras
+  brew install git-lfs
+  brew install git-quick-stats
+  brew install git-secret
+
+  git lfs install
+}
 
 p6df::modules::git::init() {
 }
