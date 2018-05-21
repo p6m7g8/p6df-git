939fc85f18ad853f5b911779556ee0df21a01d04
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-21 12:27:55 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,12 +1,12 @@
 p6df::modules::git::version() { echo "0.0.1" }
-p6df::modules::git::deps()    { 
+p6df::modules::git::deps()    {
 	ModuleDeps=(
 		sorin-ionescu/prezto:modules/git
 		p6m7g8/p6git
 	)
 }
 
-p6df::modules::git::external::brew() { 
+p6df::modules::git::external::brew() {
 
   brew install git --without-completions
   brew install git-extras
@@ -18,6 +18,35 @@ p6df::modules::git::external::brew() {
 }
 
 p6df::modules::git::init() {
+
+  p6df::modules::git::prompt
+}
+
+p6df::modules::git::prompt() {
+
+  add-zsh-hook precmd p6df::modules::git::prompt_precmd
+}
+
+p6df::modules::git::prompt_precmd() {
+  p6df::modules::git::vcs_info
+}
+
+p6df::modules::git::vcs_info() {
+  
+  if git rev-parse --is-inside-work-tree &> /dev/null; then
+    org=ORG
+    repo=REPO
+    shortsha=$(p6_git_sha_short_get)
+    branch=$(p6_git_branch_get)
+#    status=$(p6_git_dirty_get)
+  fi
+}
+
+p6df::prompt::git::line() {
+
+ if git rev-parse --is-inside-work-tree &> /dev/null; then
+   echo "git:\t$org/$repo @ $shortsha ($branch)"
+ fi
 }
 
 p6df::modules::git::init
