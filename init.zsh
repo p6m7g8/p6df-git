b2638dbab48bdcd0bcbbfd7daa89a1a6920043e4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-02 11:54:27 -0400

    feat(aliases): finalizes .gitconfig[aliases] -> p6_git_p6_*() funcs (#4)

--- init.zsh
+++ init.zsh
@@ -29,7 +29,39 @@ p6df::modules::git::home::symlink() {
 
 p6df::modules::git::init() {
 
+  p6df::modules::git::aliases::init
   p6df::modules::git::prompt
+}
+
+p6df::modules::git::aliases::init() {
+
+  ## undo aliases from sorin-ionescu/prezto:modules/git
+  ## keep the other stuff
+  local a
+  for a in $(alias | grep git | awk -F= '{ print $1 }'); do
+    unalias $a
+  done
+
+  ## my aliases (finally!)
+  alias g='p6_git_cmd'
+  alias gs='p6_git_p6_status'
+  alias gd='p6_git_p6_diff'
+  alias gl='p6_git_p6_log'
+  alias gco='p6_git_p6_checkout'
+  alias gcb='p6_git_p6_branch_create'
+  alias gb='p6_git_p6_branch'
+  alias gp='p6_git_p6_pull'
+  alias gP='p6_git_p6_push'
+  alias gPt='p6_git_p6_push_tags'
+  alias gS='p6_git_p6_sync'
+  alias gm='p6_git_p6_merge'
+  alias gc='p6_git_p6_commit'
+  alias gcl='p6_git_p6_commit_last_edit'
+  alias gf='p6_git_p6_fetch'
+  alias gc='p6_git_p6_clone'
+  alias gr='p6_git_p6_revert'
+  alias ggf='p6_git_p6_grep_files'
+  alias gg='p6_git_p6_grep'
 }
 
 p6df::modules::git::prompt() {
