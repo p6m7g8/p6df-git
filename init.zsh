8ed26d197cab40c0a3ceb9559d44ec79b93e03dd
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-06-07 15:16:32 -0400

    chore(doc): regens (#6)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- init.zsh
+++ init.zsh
@@ -1,4 +1,18 @@
+######################################################################
+#<
+#
+# Function: p6df::modules::git::version()
+#
+#>
+######################################################################
 p6df::modules::git::version() { echo "0.0.1" }
+######################################################################
+#<
+#
+# Function: p6df::modules::git::deps()
+#
+#>
+######################################################################
 p6df::modules::git::deps()    {
 	ModuleDeps=(
 		sorin-ionescu/prezto:modules/git
@@ -6,6 +20,13 @@ p6df::modules::git::deps()    {
 	)
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::git::external::brew()
+#
+#>
+######################################################################
 p6df::modules::git::external::brew() {
 
   brew install git
@@ -21,18 +42,39 @@ p6df::modules::git::external::brew() {
   git lfs install
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::git::home::symlink()
+#
+#>
+######################################################################
 p6df::modules::git::home::symlink() {
 
     ln -fs $P6_DFZ_SRC_P6M7G8_DIR/p6df-git/share/.gitconfig .gitconfig
     ln -fs $P6_DFZ_SRC_P6M7G8_DIR/p6df-git/share/.gitignore_global .gitignore_global
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::git::init()
+#
+#>
+######################################################################
 p6df::modules::git::init() {
 
   p6df::modules::git::aliases::init
   p6df::modules::git::prompt
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::git::aliases::init()
+#
+#>
+######################################################################
 p6df::modules::git::aliases::init() {
 
   ## undo aliases from sorin-ionescu/prezto:modules/git
@@ -44,35 +86,60 @@ p6df::modules::git::aliases::init() {
 
   ## my aliases (finally!)
   alias g='p6_git_cmd'
-  alias gs='p6_git_p6_status'
-  alias gd='p6_git_p6_diff'
-  alias gl='p6_git_p6_log'
-  alias gco='p6_git_p6_checkout'
-  alias gcb='p6_git_p6_branch_create'
+  alias ga='p6_git_p6_add'
+  alias ga='p6_git_p6_add_all'
   alias gb='p6_git_p6_branch'
+  alias gc='p6_git_p6_clone'
+  alias gc='p6_git_p6_commit'
+  alias gcb='p6_git_p6_branch_create'
+  alias gcl='p6_git_p6_commit_last_edit'
+  alias gco='p6_git_p6_checkout'
+  alias gcom='p6_git_p6_checkout_master'
+  alias gd='p6_git_p6_diff'
+  alias gdh='p6_git_p6_diff_head'
+  alias gf='p6_git_p6_fetch'
+  alias gg='p6_git_p6_grep'
+  alias ggf='p6_git_p6_grep_files'
+  alias gl='p6_git_p6_log'
+  alias gm='p6_git_p6_merge'
   alias gp='p6_git_p6_pull'
   alias gP='p6_git_p6_push'
   alias gPt='p6_git_p6_push_tags'
-  alias gS='p6_git_p6_sync'
-  alias gm='p6_git_p6_merge'
-  alias gc='p6_git_p6_commit'
-  alias gcl='p6_git_p6_commit_last_edit'
-  alias gf='p6_git_p6_fetch'
-  alias gc='p6_git_p6_clone'
   alias gr='p6_git_p6_revert'
-  alias ggf='p6_git_p6_grep_files'
-  alias gg='p6_git_p6_grep'
+  alias gs='p6_git_p6_status'
+  alias gS='p6_git_p6_sync'
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::git::prompt()
+#
+#>
+######################################################################
 p6df::modules::git::prompt() {
 
   add-zsh-hook precmd p6df::modules::git::prompt_precmd
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::git::prompt_precmd()
+#
+#>
+######################################################################
 p6df::modules::git::prompt_precmd() {
   p6df::modules::git::vcs_info
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::git::vcs_info()
+#
+#>
+######################################################################
 p6df::modules::git::vcs_info() {
 
     if p6_git_inside_tree; then
@@ -84,11 +151,28 @@ p6df::modules::git::vcs_info() {
     fi
 }
 
+######################################################################
+#<
+#
+# Function: p6df::prompt::git::line()
+#
+#>
+######################################################################
 p6df::prompt::git::line() {
 
     p6_git_prompt_info
 }
 
+######################################################################
+#<
+#
+# Function: str str = p6_git_prompt_info()
+#
+#  Returns:
+#	str - str
+#
+#>
+######################################################################
 p6_git_prompt_info() {
 
     local str
